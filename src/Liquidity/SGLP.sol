468e74f9c1a3e57fbc47d2651729bedbff1f82d4
Trong-Tra <traminhtrong2604@gmail.com>
2025-05-13 00:12:23 +0700

    fix: update Solidity version to ^0.8.26 in SGLP contract

--- src/Liquidity/SGLP.sol
+++ src/Liquidity/SGLP.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import "../SaigonLST/SaigonLST.sol";
 import "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
