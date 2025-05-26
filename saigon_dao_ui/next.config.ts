eb92f21be528f85f2b2659c1df04b50bd2bad9bc
Trong-Tra <traminhtrong2604@gmail.com>
2025-05-26 14:37:53 +0700

    feat: update next.config and package.json for improved image handling and dependencies

--- saigon_dao_ui/next.config.ts
+++ saigon_dao_ui/next.config.ts
@@ -2,8 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  transpilePackages: [],
+  transpilePackages: ["@reown/appkit", "@reown/appkit-adapter-wagmi"],
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.reown.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
